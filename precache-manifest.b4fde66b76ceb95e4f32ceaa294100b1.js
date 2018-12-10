690d2840e5aac529a5f0537a1d9356fa22057b57
Lurix6 <vasyapopovich23@gmail.com>
2018-12-10 12:52:21 +0200

    first commit

---
+++ precache-manifest.b4fde66b76ceb95e4f32ceaa294100b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d1fe6efeed9d60a4b1a",
+    "url": "/mysite2/static/js/runtime~main.1d1fe6ef.js"
+  },
+  {
+    "revision": "010c814e0ee9da74b61c",
+    "url": "/mysite2/static/js/main.010c814e.chunk.js"
+  },
+  {
+    "revision": "a8bbc309046c397090db",
+    "url": "/mysite2/static/js/1.a8bbc309.chunk.js"
+  },
+  {
+    "revision": "010c814e0ee9da74b61c",
+    "url": "/mysite2/static/css/main.a180f5d2.chunk.css"
+  },
+  {
+    "revision": "0bfa06d2c03b27438b5f53222041cf92",
+    "url": "/mysite2/index.html"
+  }
+];
